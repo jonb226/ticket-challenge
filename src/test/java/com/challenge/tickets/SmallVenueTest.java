d4d8bf737fb6dd22694ce0978ae0c1e7af196e4a
Jon Boerner <jon@jonboerner.com>
2017-12-22 07:55:16 -0500

    venue

---
+++ src/test/java/com/challenge/tickets/SmallVenueTest.java
@@ -0,0 +1,24 @@
+package com.challenge.tickets;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SmallVenueTest {
+
+    SmallVenue venue;
+
+    @Before
+    public void setup(){
+        venue = new SmallVenue();
+    }
+
+    @Test
+    public void hasSeats(){
+        assertThat(venue.seatsInFavoredOrder()).hasSize(200);
+        assertThat(venue.seatsInFavoredOrder().get(0)).isEqualTo(Seat.from("1A"));
+        assertThat(venue.seatsInFavoredOrder().get(199)).isEqualTo(Seat.from("20J"));
+    }
+
+}
